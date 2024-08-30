1e4a33c640623ad2ce3f5e248dc2b27973111fde
fededlV <fededelavega2@gmail.com>
2024-08-30 14:04:58 -0300

    clases en java

--- clases/src/main/java/Fraccion.java
+++ clases/src/main/java/Fraccion.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
-
 public class Fraccion {
+    private int numerador;
+    private int denominador;
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public Fraccion(int numerador, int denominador){
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    @Override
+    public String toString() {
+        return numerador + "/" + denominador;
+    }
+
+    public Fraccion(int numerador){
+        this(numerador, 1);
+    }
+
+    public Fraccion(Fraccion f){
+        this(f.numerador, f.denominador);
+    }
+
+    public double Resultado(Fraccion f){
+        double resp = f.numerador / (double) f.denominador;
+        return resp;
+    }
+
+    // Método para sumar fracciones
+    public Fraccion sumar(Fraccion otra) {
+        // Calcular el nuevo numerador
+        int nuevoNumerador = this.numerador * otra.getDenominador() + otra.getNumerador() * this.denominador;
+        // Calcular el nuevo denominador
+        int nuevoDenominador = this.denominador * otra.getDenominador();
+        // Retornar una nueva fracción con los valores calculados
+        return new Fraccion(nuevoNumerador, nuevoDenominador);
+    }
+
+    // Método para simplificar la fracción
+    private int mcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return mcd(b, a % b);
+    }
+
+
 }
--- clases/src/main/java/Prueba.java
+++ clases/src/main/java/Prueba.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Prueba {
+    public static void main(String[] args) {
+        Fraccion f1 = new Fraccion(3, 4);
+        System.out.println(f1);
+        System.out.println(f1.Resultado(f1));
+        Fraccion f2 = new Fraccion(4, 5);
+        Fraccion resultado = f1.sumar(f2);
+        System.out.println("Resultado:" + resultado);
+    }
 }
